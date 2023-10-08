23f25c8cb64c91274f06998835be829fc8e8e54e
santhoshpsk <santhoshpsk@outlook.com>
2023-10-08 06:10:00 +0000

    test

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+
 import os
+import time
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"my-node": os.getenv("node_name", "Not Found")}
+async def root(request):
+    my_var = request.session.get("my_var", None)
+    if not my_var:
+        request.session["my_var"] = "1234"
+    return {"my-node": os.getenv("node_name", "Not Found"), "my_var": str(my_var)}
