70f212e1a6c47ce86b3a2103489adfd0458302c2
santhoshpsk <santhoshpsk@outlook.com>
2023-10-08 06:43:11 +0000

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 import os
 import time
@@ -7,7 +7,7 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root(request):
+async def root(request: Request):
     my_var = request.session.get("my_var", None)
     if not my_var:
         request.session["my_var"] = "1234"
